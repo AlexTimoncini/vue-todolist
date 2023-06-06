dfb42715545616f5c430e4621ad0a5964169365d
AlexTimoncini <alextimonci@gmail.com>
2023-06-06 15:49:32 +0200

    milestone 3

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
-// Bonus:
-// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
-// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 const { createApp } = Vue
 
 createApp({
@@ -30,11 +25,18 @@ createApp({
                     done: false
                 }
             ],
+            newTask: '',
         }
     },
     methods: {
         deleteItem(itemIndex){
             this.toDoList.splice(itemIndex, 1)
+        },
+        addItem(){
+            if (this.newTask != ''){
+                this.toDoList.push({toDo: this.newTask, done: false});
+                this.newTask = '';
+            }
         }
     }
 }).mount('#app')
